b55c785a2ea21ede5e404c1b3769270a1f1b2e76
DiSLucky7 <nagyklevi@gmail.com>
2022-06-10 23:44:31 +0200

    Calorie-chart-editor component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GenericDataTableComponent } from './generic-table/generic-data-table/generic-data-table.component';
 import { GenericTableModule } from './generic-table/generic-table.module';
+import { CalorieChartEditorComponent } from './page/calorie-chart-editor/calorie-chart-editor.component';
 import { CalorieChartComponent } from './page/calorie-chart/calorie-chart.component';
 
 const routes: Routes = [
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'calorie-chart',
     component: CalorieChartComponent,
+  },
+  {
+    path: 'calorie-chart/:_id',
+    loadChildren: () => import('./generic-table/generic-table.module').then(m => m.GenericTableModule)
+    // component: CalorieChartEditorComponent,
+    // children :[
+    //   { path: '', component: GenericDataTableComponent},
+  // ]
   },
 ];
 
